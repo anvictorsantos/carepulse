e3deba2aa35a7af65d0217148846d770a828e660
Antonio Pontes <anvictorsantos@gmail.com>
2024-09-21 08:45:45 +0100

    feat: absolute path imports
    WIP: appointment table

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,27 +3,12 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import StatCard from '@/components/StatCard';
-import { columns, Payment } from '@/components/table/columns';
+import { columns } from '@/components/table/columns';
 import { DataTable } from '@/components/table/DataTable';
 import { getRecentAppointmentList } from '@/lib/actions/appointment.actions';
 
-async function getData(): Promise<Payment[]> {
-    // Fetch data from your API here.
-    return [
-        {
-            id: '728ed52f',
-            amount: 100,
-            status: 'pending',
-            email: 'm@example.com',
-        },
-        // ...
-    ];
-}
-
 const Admin = async () => {
     const appointments = await getRecentAppointmentList();
-
-    const data = await getData();
 
     return (
         <div className="mx-auto flex max-w-7xl flex-col space-y-14">
@@ -70,8 +55,7 @@ const Admin = async () => {
                     />
                 </section>
 
-                {/* <DataTable columns={columns} data={appointments.documents} /> */}
-                <DataTable columns={columns} data={data} />
+                <DataTable columns={columns} data={appointments.documents} />
             </main>
         </div>
     );
--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -1,78 +1,92 @@
 'use client';
 import { ColumnDef } from '@tanstack/react-table';
-import { MoreHorizontal } from 'lucide-react';
+import Image from 'next/image';
 
-import { Button } from '@/components/ui/button';
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+import AppointmentModal from '@/components/AppointmentModal';
+import StatusBadge from '@/components/StatusBadge';
+import { Doctors } from '@/constants';
+import { formatDateTime } from '@/lib/utils';
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type Payment = {
-    id: string;
+export type Patient = {
     amount: number;
-    status: 'pending' | 'processing' | 'success' | 'failed';
     email: string;
+    patient: {
+        name: string;
+    };
+    primaryPhysician: string;
+    id: string;
+    schedule: Date;
+    status: 'pending' | 'scheduled' | 'cancelled';
 };
 
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<Patient>[] = [
+    {
+        header: 'ID',
+        cell: ({ row }) => <p className="text-14-medium">{row.index + 1}</p>,
+    },
+    {
+        accessorKey: 'patient',
+        header: 'Patient',
+        cell: ({ row }) => {
+            return (
+                <p className="text-14-medium">{row.original.patient.name}</p>
+            );
+        },
+    },
     {
         id: 'actions',
+        header: () => <div className="pl-4">Action</div>,
         cell: ({ row }) => {
-            const payment = row.original;
-
+            console.log(row);
             return (
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem
-                            onClick={() =>
-                                navigator.clipboard.writeText(payment.id)
-                            }
-                        >
-                            Copy payment ID
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>View customer</DropdownMenuItem>
-                        <DropdownMenuItem>
-                            View payment details
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
+                <div className="flex gap-1">
+                    <AppointmentModal />
+                </div>
             );
         },
     },
     {
         accessorKey: 'status',
         header: 'Status',
-    },
-    {
-        accessorKey: 'email',
-        header: 'Email',
-    },
-    {
-        accessorKey: 'amount',
-        header: () => <div className="text-right">Amount</div>,
         cell: ({ row }) => {
-            const amount = parseFloat(row.getValue('amount'));
-            const formatted = new Intl.NumberFormat('en-US', {
-                style: 'currency',
-                currency: 'USD',
-            }).format(amount);
+            <div className="min-w-[115px]">
+                <StatusBadge status={row.original.status} />
+            </div>;
+        },
+    },
+    {
+        accessorKey: 'schedule',
+        header: 'Appointment',
+        cell: ({ row }) => (
+            <p className="text-14-regular min-w-[100px]">
+                {formatDateTime(row.original.schedule).dateTime}
+            </p>
+        ),
+    },
+    {
+        accessorKey: 'primaryPhysician',
+        header: 'Doctor',
+        cell: ({ row }) => {
+            const doctor = Doctors.find(
+                (doc) => doc.name === row.original.primaryPhysician,
+            );
 
-            return <div className="text-right font-medium">{formatted}</div>;
+            return (
+                <div className="flex items-center gap-3">
+                    <Image
+                        src={doctor?.image ?? '/assets/images/dr-cruz.png'}
+                        alt={doctor?.name ?? 'Dr. Cruz'}
+                        width={100}
+                        height={100}
+                        className="size-8"
+                    />
+                    <p className="whitespace-nowrap">
+                        Dr. {doctor?.name ?? 'Cruz'}
+                    </p>
+                </div>
+            );
         },
     },
 ];
