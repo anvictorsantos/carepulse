49a4399acc34fe76d5269e4949d12c771c594a97
Antonio Pontes <anvictorsantos@gmail.com>
2024-10-05 09:11:20 +0100

    feat: enable sentry analytics

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+    dsn: 'https://ac10cbe858bf578f2a112a25ec069533@o4508069056741376.ingest.de.sentry.io/4508069060542544',
+
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
+
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+});
